4a58c70716503eebeab81dae26d7b2d6a70d0b49
dharmrajzine <dharmrajzine@gmail.com>
2024-05-27 19:11:25 +0530

    Day 7 Javascript Programs Added

---
+++ Day-7 JavaScript/02Basics/01Variable.js
@@ -0,0 +1,21 @@
+var fullName = "Dharmraj Zine";
+
+//how to name variables
+
+//fullName
+//full-name
+//full_name
+//FullName
+
+var courseName = "React JS Bootcamp";
+var isLoggedIn = false;
+
+var loggedCount =32;
+
+// console.log(loggedCount);
+// console.log("loggedCount");
+
+var paymentMode;
+
+paymentMode = "Credit Card";
+console.log(paymentMode);
---
+++ Day-7 JavaScript/02Basics/02Variables-two.js
@@ -0,0 +1,24 @@
+const uid ="abc123";
+
+var fullName = "Dharmraj Zine";
+var email = "dharmajzine@gmail.com";
+var password = "123456";
+var confirmPassword = "123456";
+var courseCount = 0;
+var isLoggedInFromGoogle = false;
+
+// console.log(uid);
+// console.log(fullName);
+// console.log(email);
+// console.log(password);
+// console.log(confirmPassword);
+// console.log(courseCount);
+// console.log(isLoggedInFromGoogle);
+
+console.log(`
+With Unique ID: ${uid}
+User is: ${fullName}
+and his email is: ${email}
+and uses the password: ${password}
+`)
+
