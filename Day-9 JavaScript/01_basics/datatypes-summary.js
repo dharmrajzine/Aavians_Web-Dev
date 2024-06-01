a16711102492d6049a59f404242782e2f5d124b5
dharmrajzine <dharmrajzine@gmail.com>
2024-06-01 23:47:59 +0530

    Day 9 JavaScript Programs

---
+++ Day-9 JavaScript/01_basics/datatypes-summary.js
@@ -0,0 +1,59 @@
+// Primitive
+
+// 7 types : string, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false;
+const outSideTemp = null;
+let userEmail;
+
+const id = Symbol('123')
+const anotherId =Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 1312412487218742182193n
+
+//Reference (Non Primitive)
+
+//Array, Objects, Functions
+
+const heros =["shaktiman","naagraj","dosa"];
+let myObj = {
+    name:"dharmraj",
+    age:22,
+}
+console.log(myObj)
+
+const myFunction =function(){
+    console.log("Hello world");
+}
+myFunction();
+
+console.log(typeof myFunction);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// ************** 
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = "dharmrajzinedotcom";
+
+let anothername = myYoutubename;
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi:"user@ybl",
+}
+
+let userTwo = userOne;
+
+userTwo.email = "*****@***.***";
+
+console.log(userOne.email);
+console.log(userTwo.email);
